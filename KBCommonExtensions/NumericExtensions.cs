99324beda06a0d7e901770345dccd869eb076ef7
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-10-11 13:54:58 +0530

    KBCommonExtensions
    Generic and Common Extensions which make development faster.

---
+++ KBCommonExtensions/NumericExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KBCommonExtensions
+{
+    public static class NumericExtensions
+    {
+        /// <summary>
+        /// Seperates the number with comma (Thousand Seperator)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static string ThousandSeparator<T>(T obj)
+        {
+            string seperator = string.Empty;
+            if(obj != null) { 
+                seperator = string.Format("{0:n}", obj);
+            }
+            return seperator;
+        }
+    }
+}
---
+++ KBCommonExtensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Linq;
+
+namespace KBCommonExtensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Generates random password based on length
+        /// </summary>
+        /// <param name="value">String Length</param>
+        /// <returns></returns>
+        public static string RandomPassword(int value)
+        {
+            Random rnd = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]";
+            return new string(Enumerable.Repeat(chars, value)
+              .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+
+        /// <summary>
+        /// Randoms the Fake paragraph text.
+        /// </summary>
+        /// <param name="value">Number of lines to count. default is 1.</param>
+        /// <returns></returns>
+        public static string RandomParagraphText(int value = 1)
+        {
+            string repeatedString = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.";
+            return String.Join(Environment.NewLine, Enumerable.Repeat(repeatedString, value));
+        }
+    }
+}
